7344268e60390fab7fe88aa6dc0b0f42333fe7b9
Martin Schurz <Martin.Schurz@t-systems.com>
2021-02-22 12:37:02 +0100

    initial commit

---
+++ pam-tester.py
@@ -0,0 +1,28 @@
+import sys
+import pam
+import click
+
+
+@click.command()
+@click.option('--user', default='root', help='username for authentication.')
+@click.option('--password', prompt=True, hide_input=True,
+              help='Password of the user.')
+@click.option('--stack', default='login', help='PAM stack to test.')
+@click.option('--expectfail', is_flag=True,
+              help='invert return code (True if PAM stack failed, False if success).')
+def pam_auth(user, password, stack, expectfail):
+    """A basic testing programm fpr PAM tests."""
+    failed = True
+
+    p = pam.pam()
+    if p.authenticate(user, password, service=stack):
+        failed = False
+
+    click.echo(f'authenticating user { user } in PAM stack {stack}, status: PAM code {p.code}, PAM reason {p.reason}')
+    if (not expectfail and not failed) or (expectfail and failed):
+        sys.exit(0)
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    pam_auth()
---
+++ pam-tester.spec
@@ -0,0 +1,30 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+block_cipher = None
+
+
+a = Analysis(['pam-tester.py'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          [],
+          name='pam-tester',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          upx_exclude=[],
+          runtime_tmpdir=None,
+          console=True )
