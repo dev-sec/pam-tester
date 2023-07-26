c423b26eb29246661c0202f1bef1567358dfcd47
Martin Schurz <Martin.Schurz@t-systems.com>
2023-07-26 12:01:16 +0200

    add testing
    Signed-off-by: Martin Schurz <Martin.Schurz@t-systems.com>

--- pam_tester/pam_tester.py
+++ pam_tester/pam_tester.py
@@ -1,15 +1,20 @@
 import sys
-import pam
+
 import click
+import pam
 
 
 @click.command()
-@click.option('--user', default='root', help='username for authentication.')
-@click.option('--password', prompt=True, hide_input=True,
-              help='Password of the user.')
-@click.option('--stack', default='login', help='PAM stack to test.')
-@click.option('--expectfail', is_flag=True,
-              help='invert return code (True if PAM stack failed, False if success).')
+@click.option("--user", default="root", help="username for authentication.")
+@click.option(
+    "--password", prompt=True, hide_input=True, help="Password of the user."
+)
+@click.option("--stack", default="login", help="PAM stack to test.")
+@click.option(
+    "--expectfail",
+    is_flag=True,
+    help="invert return code (True if PAM stack failed, False if success).",
+)
 def pam_auth(user, password, stack, expectfail):
     """A basic testing programm for PAM tests."""
     failed = True
@@ -18,11 +23,15 @@ def pam_auth(user, password, stack, expectfail):
     if p.authenticate(user, password, service=stack):
         failed = False
 
-    click.echo(f'authenticating user { user } in PAM stack {stack}, status: PAM code {p.code}, PAM reason {p.reason}')
-    # we expect no error (false) and it errors (true) -> not false and true -> true
-    # we expect no error (false) and it does not error (false) -> not false and false -> false
-    # we expect an error (true) and it errors (true) -> not true and true -> false
-    # we expect an error (true) and it does not error (false) -> not true and false -> false
+    click.echo(
+        f"authenticating user { user } in PAM stack {stack}, "
+        f"status: PAM code {p.code}, PAM reason {p.reason}"
+    )
+    # we expect 
+    # no error (false) and it errors (true) -> not false and true -> true
+    # no error (false) and it does not error (false) -> not false and false -> false
+    # an error (true) and it errors (true) -> not true and true -> false
+    # an error (true) and it does not error (false) -> not true and false -> false
     if (not expectfail and failed) or (expectfail and not failed):
         sys.exit(1)
     sys.exit(0)
